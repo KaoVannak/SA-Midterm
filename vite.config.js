794a82e0efe7b26576c083d4547e83db2eff9a00
Kao Vannak <kaovannak009@gmail.com>
2025-01-31 19:27:23 +0700

    Fix routing issueses

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SA-Midterm',
   plugins: [vue()],
-})
+  base: '/SA-Midterm/', // Replace this with your repo name
+});
+
+
